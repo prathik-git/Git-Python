dd9a47254f8cd9e2df98cec5d6522175b0a7aa9f
Prathik Bhat <prathik@gmail.com>
2023-04-26 12:56:10 +0530

    updating recent change

---
+++ Lists.py
@@ -0,0 +1,14 @@
+nums = [25,20,30,15,45]
+names = ['prathik', 'sumit', 'navin']
+values = [9.5, 'prathik', 45]
+mil = [nums, names, values]
+print(mil)
+nums.append(45)
+nums.insert(2,75)
+print(nums)
+nums.pop()
+min(nums)
+max(nums)
+nums.sort()
+print(nums)
+
