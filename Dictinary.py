7f73184d7cca7147514ff156ca6e9ff0052b2026
Prathik Bhat <prathik@gmail.com>
2023-05-01 13:40:38 +0530

    "Adding Dictionary"

---
+++ Dictinary.py
@@ -0,0 +1,7 @@
+dic = {1:'Prathik', 2:'Sumit', 4:'Navin'}
+dic.get(3,'not found')
+keys = ['prathik', 'sumit', 'navin']
+values = ['python', 'java', 'JS']
+data = dict(zip(keys,values))
+data['Monika'] = 'CSharp'
+data
